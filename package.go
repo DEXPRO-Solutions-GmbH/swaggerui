03a8e0780414f86bac75bd46e2cf5835c40e28bc
FabianTe <fabiante@users.noreply.github.com>
2023-07-04 21:48:30 +0200

    Add package documentation

---
+++ package.go
@@ -0,0 +1,4 @@
+// Package swaggerui allows you to serve a Swagger UI.
+//
+// The subdirectory dist contains a prebuild version of the Swagger UI taken from the official Github page.
+package swaggerui
