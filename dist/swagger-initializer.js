602e31fc6f06ebcda0485d8934da611d2ef7eed3
fabiante <fabiante@users.noreply.github.com>
2024-11-05 09:02:45 +0100

    Upgrade SwaggerUI to v5.17.14
    Taken from: https://github.com/swagger-api/swagger-ui/releases/tag/v5.17.14

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -2,12 +2,8 @@ window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
-
-  var openApiUrl = document.URL.substr(0,document.URL.indexOf("swagger-ui/")) + "openapi.yml";
-  console.debug("fetching open api", openApiUrl);
-
   window.ui = SwaggerUIBundle({
-    url: openApiUrl,
+    url: "https://petstore.swagger.io/v2/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
